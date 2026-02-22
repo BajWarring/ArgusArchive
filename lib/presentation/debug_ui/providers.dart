b088d0572135033a9790e6b2fb09533a06e71cf8
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-22 17:00:16 +0530

    Update providers.dart

--- lib/presentation/debug_ui/providers.dart
+++ lib/presentation/debug_ui/providers.dart
@@ -4,6 +4,9 @@ import '../../core/interfaces/storage_adapter.dart';
 import '../../core/models/file_entry.dart';
 import '../../features/file_handlers/file_handler_registry.dart';
 import '../../features/file_handlers/image_handler.dart';
+import '../../features/file_handlers/text_handler.dart';
+import '../../features/file_handlers/svg_handler.dart';
+import '../../features/file_handlers/pdf_handler.dart';
 
 /// 1. Make the adapter a StateProvider so we can swap it to a ZIP adapter at runtime
 final storageAdapterProvider = StateProvider<StorageAdapter>((ref) {
@@ -34,9 +37,11 @@ final directoryContentsProvider = FutureProvider.autoDispose<List<FileEntry>>((r
 final fileHandlerRegistryProvider = Provider<FileHandlerRegistry>((ref) {
   final registry = FileHandlerRegistry();
   
-  // Register our handlers here. The first one that returns true for 'canHandle' wins.
+  // Handlers registered first have priority.
   registry.register(ImageHandler());
-  // You can add DocumentHandler(), AudioHandler(), etc., here later.
+  registry.register(SvgHandler());
+  registry.register(PdfHandler());
+  registry.register(TextHandler());
   
   return registry;
 });
