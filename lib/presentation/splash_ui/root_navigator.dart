f7492da8c997f36b535e256c5c091561b4b06a9d
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-24 22:11:27 +0530

    Update root_navigator.dart

--- lib/presentation/splash_ui/root_navigator.dart
+++ lib/presentation/splash_ui/root_navigator.dart
@@ -1,22 +1,12 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import '../../providers/permission_provider.dart';
-import '../debug_ui/file_browser_debug.dart'; // We will swap this for the Production UI later
-import 'permission_screen.dart';
+import '../main_ui/main_screen.dart';
 
-class RootNavigator extends ConsumerWidget {
+class RootNavigator extends StatelessWidget {
   const RootNavigator({super.key});
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    // Watch the current permission state
-    final hasPermission = ref.watch(storagePermissionProvider);
-
-    // If permission is granted, go to the file browser. Otherwise, show the permission screen.
-    if (hasPermission) {
-      return const FileBrowserDebug();
-    } else {
-      return const PermissionScreen();
-    }
+  Widget build(BuildContext context) {
+    // This immediately launches the new File Manager UI you just pasted!
+    return const MainScreen();
   }
 }
