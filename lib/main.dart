9bd12347512b466844b7e2b72975c2d02f160f9f
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-24 15:31:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'presentation/splash_ui/root_navigator.dart';
+import 'services/sub_app/shortcut_service.dart';
+import 'features/video_player_app/video_library_screen.dart';
+
+// Global Key to allow navigation from outside the standard widget tree
+final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -11,12 +16,42 @@ void main() {
   );
 }
 
-class ArgusArchiveApp extends StatelessWidget {
+class ArgusArchiveApp extends StatefulWidget {
   const ArgusArchiveApp({super.key});
+
+  @override
+  State<ArgusArchiveApp> createState() => _ArgusArchiveAppState();
+}
+
+class _ArgusArchiveAppState extends State<ArgusArchiveApp> {
+  
+  @override
+  void initState() {
+    super.initState();
+    _initShortcuts();
+  }
+
+  Future<void> _initShortcuts() async {
+    // 1. Check if the app was launched FROM a shortcut
+    final initialRoute = await ShortcutService.getInitialRoute();
+    if (initialRoute == '/video_library') {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => const VideoLibraryScreen()));
+      });
+    }
+
+    // 2. Listen for shortcut taps while the app is already open in the background
+    ShortcutService.listenToRouteChanges((route) {
+      if (route == '/video_library') {
+        navigatorKey.currentState?.push(MaterialPageRoute(builder: (_) => const VideoLibraryScreen()));
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      navigatorKey: navigatorKey,
       title: 'Argus Archive',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
@@ -28,7 +63,6 @@ class ArgusArchiveApp extends StatelessWidget {
           elevation: 0,
         ),
       ),
-      // Set the home to our new RootNavigator
       home: const RootNavigator(),
     );
   }
