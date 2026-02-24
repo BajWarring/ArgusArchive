fb92c8049675a0c851e2ce08f98c3ff8559df3aa
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-24 21:46:33 +0530

    Create ui_theme.dart

---
+++ lib/presentation/ui_theme.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ArgusColors {
+  static const Color primary = Color(0xFF135BEC);
+  static const Color bgDark = Color(0xFF101622);
+  static const Color surfaceDark = Color(0xFF1D2636);
+  static const Color bgLight = Color(0xFFF6F6F8);
+  static const Color textDark = Color(0xFF0F172A); // slate-900
+  static const Color textLight = Color(0xFFF1F5F9); // slate-100
+}
