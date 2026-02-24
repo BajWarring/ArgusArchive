ed8343afd61636e34abe53f7eaa9e198cbe76f65
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-02-24 08:44:36 +0530

    Create VideoPlayerViewFactory.kt

---
+++ android/app/src/main/kotlin/com/app/argusarchive/VideoPlayerViewFactory.kt
@@ -0,0 +1,14 @@
+package com.app.argusarchive
+
+import android.content.Context
+import io.flutter.plugin.common.BinaryMessenger
+import io.flutter.plugin.common.StandardMessageCodec
+import io.flutter.plugin.platform.PlatformView
+import io.flutter.plugin.platform.PlatformViewFactory
+
+class VideoPlayerViewFactory(private val messenger: BinaryMessenger) : PlatformViewFactory(StandardMessageCodec.INSTANCE) {
+    override fun create(context: Context, viewId: Int, args: Any?): PlatformView {
+        val creationParams = args as Map<String?, Any?>?
+        return NativeVideoPlayer(context, viewId, creationParams, messenger)
+    }
+}
