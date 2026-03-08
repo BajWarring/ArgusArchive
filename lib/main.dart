a734a9f08cfc4631e2c3c02f06ae3a7c4d43a2f8
BajWarring <91818323+BajWarring@users.noreply.github.com>
2026-03-08 17:18:12 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:media_kit/media_kit.dart';
 import 'presentation/splash_ui/root_navigator.dart';
 import 'services/sub_app/shortcut_service.dart';
-import 'presentation/subapp_video_ui/video_library_screen.dart';
+// FIXED: Points to the new folder!
+import 'services/video_player_app/video_library_screen.dart'; 
 
 final navigatorKey = GlobalKey<NavigatorState>();
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
+  MediaKit.ensureInitialized();
   runApp(
     const ProviderScope(
       child: ArgusArchiveApp(),
